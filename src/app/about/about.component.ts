663f76b213a7d816ef5bbb2385db6d118c971ea7
Ricardo Leon <ricardo.leon@ar.ey.com>
2019-05-28 17:53:18 -0300

    Videos from 1 to 9

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { interval, timer, fromEvent, Observable, noop } from 'rxjs';
+import { createHttpObservable } from '../common/util';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'about',
@@ -10,6 +13,38 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    const http$ = createHttpObservable('/api/courses');
+
+    const courses$ = http$
+      .pipe(
+        map(res => Object.values(res['payload']))
+      );
+
+    courses$.subscribe(
+      courses => console.log(courses),
+      noop,
+      () => console.log('completed')
+    );
+
+  }
+  
+  observableExamples() {
+    // This is only a definition
+    const interval$ = interval(1000);
+    // To transform it into a Stream, you should subscribe to it
+    const sub = interval$.subscribe(val => console.log("Stream 1 => " + val));
+    setTimeout(() => sub.unsubscribe(), 5000);
+  
+    const intervalTimer$ = timer(3000, 1000);
+    intervalTimer$.subscribe(val => console.log("Stream 2 => " + val));
+  
+    const click$ = fromEvent(document, 'click');
+    click$.subscribe(
+      evt => console.log(evt),
+      err => console.log(err),
+      () => console.log('completed')
+    );
   }
 
 }
