9df6a52fb6eb33c47548767c9fccc7bc6b40dfb9
Ricardo Leon <ricardo.leon@ar.ey.com>
2019-06-03 14:58:23 -0300

    Video 23

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -15,6 +15,7 @@ import {
 } from 'rxjs/operators';
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import { createHttpObservable } from '../common/util';
 
 
 @Component({
@@ -24,7 +25,9 @@ import {Lesson} from '../model/lesson';
 })
 export class CourseComponent implements OnInit, AfterViewInit {
 
-
+    courseId: string;
+    course$: Observable<Course>;
+    lessons$: Observable<Lesson[]>
 
     @ViewChild('searchInput') input: ElementRef;
 
@@ -35,20 +38,36 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     ngOnInit() {
 
-        const courseId = this.route.snapshot.params['id'];
+        this.courseId = this.route.snapshot.params['id'];
 
+        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
 
+        this.lessons$ = this.loadLessons();
 
     }
 
     ngAfterViewInit() {
 
+        const searchLessons$ = fromEvent<any>(this.input.nativeElement, 'keyup')
+            .pipe(
+                map(event => event.target.value),
+                debounceTime(400),
+                distinctUntilChanged(),
+                switchMap(search => this.loadLessons(search))
+            );
 
+        const initialLessons$ = this.loadLessons();
 
+        this.lessons$ = concat(initialLessons$, searchLessons$);
 
     }
 
-
+    loadLessons(search: string = ''): Observable<Lesson[]> {
+        return createHttpObservable(`/api/lessons?courseId=${this.courseId}&pageSize=100&filter=${search}`)
+            .pipe(
+                map(res => res['payload'])
+            );
+    }
 
 
 }
